5394921d31f2cf8fa3704685680a6d434d28f5e2
Tom <tompkin09.05.21@gmail.com>
2021-01-14 17:48:42 +0900

    2021/01/14 勉強分

--- resize_img.py
+++ resize_img.py
@@ -6,14 +6,15 @@ new_resized_list = []
 
 with open('./resized_list.txt') as f:
 	resized_img_names = [s.strip() for s in f.readlines()]
-	print(file_names)
+	# print(file_names)
 
 	for n in file_names:
-		print(n)
+		# print(n)
 		if n in resized_img_names:
 			print("リサイズ済：{}".format(n))
 			continue
 		if n[0] == "i": # ファイルが写真であることを確認してリサイズ
+			print(n)
 			image = Image.open("./ECG.assets/{}".format(n))
 			resize_img = image.resize((int(image.width/2), int(image.height/2)), Image.LANCZOS)
 			resize_img.save("./ECG.assets/{}".format(n))
